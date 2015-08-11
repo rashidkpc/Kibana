b431960afad06803c397a4a39a74d03c33f29c3e
Rashid Khan <github.fliplap@spamgourmet.com>
2015-08-11 08:38:57 -0700

    move to installedPlugins

--- src/plugins/timelion/init.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (server) {
-  //var config = server.config();
-  server.route({
-    method: 'POST',
-    path: '/timelion/sheet',
-    handler: require('./routes/sheet.js')
-  });
-
-};
